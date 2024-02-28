88b352b624443f6c71a7c7eabb52b26aeef88218
sugarbalazs <sugarbalazs94@gmail.com>
2024-02-28 09:39:42 +0100

    Changing size to make it bigger

--- project/script.js
+++ project/script.js
@@ -4,12 +4,20 @@ let mainContainer = document.getElementById("mainContainer");
 // Creating a loop to get the divs inside the main div
 for (let i = 0; i < 16; i++) {
   for (let j = 0; j < 16; j++) {
-    // Create each divs
+    // Create each div
     let cell = document.createElement("div");
     // Adding a class
     cell.className = "flex-item";
 
     // Appending the cells
     mainContainer.appendChild(cell);
+
+    cell.addEventListener(
+      "mouseenter",
+      (e) => {
+        e.target.style.color = "black";
+      },
+      false
+    );
   }
 }
